79f413d381b73b9dce52be5fe0af4ffffb5070f0
Andy Niccolai <andy.niccolai@gmail.com>
2019-07-01 15:05:29 -0700

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,4 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-  get 'welcome/index'
-
-  resources :articles
-
-  root 'welcome#index'
-
 end
