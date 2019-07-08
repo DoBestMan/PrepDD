0d705911b915a6f6107299c51c05e92ebe8f3da7
khs125 <eden.net120@gmail.com>
2019-07-09 01:16:34 +0500

    Use base64 32 encryption_key

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,9 +2,9 @@ class Company < ApplicationRecord
   belongs_to :parent, :class_name => 'Company', optional: true
   has_many :children, :class_name => 'Company', :foreign_key => 'parent_id'
 
-  after_create :generate_encryption_key
+  before_create :generate_encryption_key
 
   def generate_encryption_key
-    self.encryption_key = SecureRandom.random_bytes(32)
+    self.encryption_key = SecureRandom.urlsafe_base64(32)
   end
 end
