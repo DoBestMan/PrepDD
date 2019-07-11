0ca832d3f7525992759eee26a9914820a6a67c7f
khs125 <eden.net120@gmail.com>
2019-07-12 02:42:53 +0500

    Add mutation for user detail update

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < GraphQL::Schema::Object
+    field :updateUserDetails, mutation: Mutations::UpdateUserDetails
     field :sign_in_user, mutation: Mutations::SignInUser
     field :sign_up_user, mutation: Mutations::SignUpUser
     field :sign_out_user, mutation: Mutations::SignOutUser
