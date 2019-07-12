7f547431c298153a0c4511039ef6eaa4a18f8b04
khs125 <eden.net120@gmail.com>
2019-07-13 02:08:50 +0500

    Update team management table

--- app/javascript/src/team/TeamManagement.tsx
+++ app/javascript/src/team/TeamManagement.tsx
@@ -1,175 +1,92 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
+import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import { deepPurple } from '@material-ui/core/colors';
+// import SearchBar from 'material-ui-search-bar'
 
-const useStyles1 = makeStyles(theme => ({
+
+const useStyles = makeStyles(theme => ({
     root: {
-        flexShrink: 0,
-        color: theme.palette.text.secondary,
-        marginLeft: theme.spacing(2.5),
+        width: '80%',
+        marginLeft: '10%',
+        marginTop: theme.spacing(3),
+        alignItems: 'center',
+        overflowX: 'auto',
     },
+    table: {
+        minWidth: 650,
+    },
+    avatar: {
+        display: 'inline'
+    },
+    avatar: {
+        margin: 10,
+    },
+    purpleAvatar: {
+        margin: 10,
+        color: '#fff',
+        backgroundColor: deepPurple[500],
+    },
+    nameCustom: {
+        marginLeft: 20,
+        marginTop: 15,
+    },
+    avatarCustom: {
+        display: 'inline-flex',
+        width: '100%',
+    },
+
 }));
 
-function TablePaginationActions(props) {
-    const classes = useStyles1();
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onChangePage } = props;
-
-    function handleFirstPageButtonClick(event) {
-        onChangePage(event, 0);
-    }
-
-    function handleBackButtonClick(event) {
-        onChangePage(event, page - 1);
-    }
-
-    function handleNextButtonClick(event) {
-        onChangePage(event, page + 1);
-    }
-
-    function handleLastPageButtonClick(event) {
-        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    }
-
-    return (
-        <div className={classes.root}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="First Page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="Next Page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="Last Page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </div>
-    );
-}
-
-TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onChangePage: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-};
-
-function createData(name, calories, fat) {
-    return { name, calories, fat };
+function createData(name, team, role, company) {
+    return { name, team, role, company };
 }
 
 const rows = [
-    createData('Cupcake', 305, 3.7),
-    createData('Donut', 452, 25.0),
-    createData('Eclair', 262, 16.0),
-    createData('Frozen yoghurt', 159, 6.0),
-    createData('Gingerbread', 356, 16.0),
-    createData('Honeycomb', 408, 3.2),
-    createData('Ice cream sandwich', 237, 9.0),
-    createData('Jelly Bean', 375, 0.0),
-    createData('KitKat', 518, 26.0),
-    createData('Lollipop', 392, 0.2),
-    createData('Marshmallow', 318, 0),
-    createData('Nougat', 360, 19.0),
-    createData('Oreo', 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+    createData('User One', 'PrepDD', 'Admin', 'PrepDD'),
+    createData('User Two', 'design', 'Member', 'PrepDD'),
+    createData('User Three', 'SS', 'Member', 'PrepDD'),
+];
 
-const useStyles2 = makeStyles(theme => ({
-    root: {
-        width: '100%',
-        marginTop: theme.spacing(3),
-    },
-    table: {
-        minWidth: 500,
-    },
-    tableWrapper: {
-        overflowX: 'auto',
-    },
-}));
-
-export default function CustomPaginationActionsTable() {
-    const classes = useStyles2();
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
-    function handleChangePage(event, newPage) {
-        setPage(newPage);
-    }
-
-    function handleChangeRowsPerPage(event) {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    }
+export default function SimpleTable() {
+    const classes = useStyles();
 
     return (
         <Paper className={classes.root}>
-            <div className={classes.tableWrapper}>
-                <Table className={classes.table}>
-                    <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
-                            <TableRow key={row.name}>
-                                <TableCell component="th" scope="row">
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Company(s)</TableCell>
+                        <TableCell align="right">Team&nbsp;(g)</TableCell>
+                        <TableCell align="right">Role&nbsp;(g)</TableCell>
+                        <TableCell align="right"></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map(row => (
+                        <TableRow key={row.name}>
+                            <TableCell className={classes.avatarCustom} component="th" scope="row">
+                                <Avatar className={classes.purpleAvatar}>A</Avatar>
+                                <Typography className={classes.nameCustom} variant="body2" gutterBottom>
                                     {row.name}
-                                </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                            </TableRow>
-                        ))}
-
-                        {emptyRows > 0 && (
-                            <TableRow style={{ height: 48 * emptyRows }}>
-                                <TableCell colSpan={6} />
-                            </TableRow>
-                        )}
-                    </TableBody>
-                    <TableFooter>
-                        <TableRow>
-                            <TablePagination
-                                rowsPerPageOptions={[5, 10, 25]}
-                                colSpan={3}
-                                count={rows.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                SelectProps={{
-                                    inputProps: { 'aria-label': 'Rows per page' },
-                                    native: true,
-                                }}
-                                onChangePage={handleChangePage}
-                                onChangeRowsPerPage={handleChangeRowsPerPage}
-                                ActionsComponent={TablePaginationActions}
-                            />
+                                </Typography>
+                            </TableCell>
+                            <TableCell align="right">{row.company}</TableCell>
+                            <TableCell align="right">{row.team}</TableCell>
+                            <TableCell align="right">{row.role}</TableCell>
+                            <TableCell align="right">Click</TableCell>
                         </TableRow>
-                    </TableFooter>
-                </Table>
-            </div>
+                    ))}
+                </TableBody>
+            </Table>
         </Paper>
     );
 }
