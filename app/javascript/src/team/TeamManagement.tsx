b16ea8e60e4fe4d2a31cb1e354dfec67f35c1963
khs125 <eden.net120@gmail.com>
2019-07-16 01:52:54 +0500

    Add searchbar and selecter in team management page

--- app/javascript/src/team/TeamManagement.tsx
+++ app/javascript/src/team/TeamManagement.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -9,8 +9,22 @@ import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import { deepPurple } from '@material-ui/core/colors';
-// import SearchBar from 'material-ui-search-bar'
+import Button from '@material-ui/core/Button';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const options = [
+    'Parent Company',
+    'Broker',
+    'Assign broker',
+];
+
+const ITEM_HEIGHT = 48;
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,6 +33,9 @@ const useStyles = makeStyles(theme => ({
         marginTop: theme.spacing(3),
         alignItems: 'center',
         overflowX: 'auto',
+    },
+    button: {
+        margin: theme.spacing(1),
     },
     table: {
         minWidth: 650,
@@ -40,6 +57,46 @@ const useStyles = makeStyles(theme => ({
         display: 'inline-flex',
         width: '100%',
     },
+    search: {
+        // display: 'inline',
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        border: '1px solid lightgray',
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 300,
+        },
+    },
+    selectDrop: {
+        display: 'inline',
+    }
 
 }));
 
@@ -55,9 +112,69 @@ const rows = [
 
 export default function TeamManagement(props: {path?: string}) {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    function handleClick(event) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleClose() {
+        setAnchorEl(null);
+    }
 
     return (
         <Paper className={classes.root}>
+
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                    <SearchIcon />
+                </div>
+                <InputBase
+                    placeholder="Search…"
+                    classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                    }}
+                    inputProps={{ 'aria-label': 'Search' }}
+                />
+
+                <Button variant="contained" color="primary" className={classes.button}>
+                    Add member
+                </Button>
+
+
+                <div className={classes.selectDrop}>
+                    <IconButton
+                        aria-label="More"
+                        aria-controls="long-menu"
+                        aria-haspopup="true"
+                        onClick={handleClick}
+                    >
+                        <MoreVertIcon />
+                    </IconButton>
+                    <Menu
+                        id="long-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={open}
+                        onClose={handleClose}
+                        PaperProps={{
+                            style: {
+                                maxHeight: ITEM_HEIGHT * 4.5,
+                                width: 200,
+                            },
+                        }}
+                    >
+                        {options.map(option => (
+                            <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+                                {option}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </div>
+            </div>
+
             <Table className={classes.table}>
                 <TableHead>
                     <TableRow>
