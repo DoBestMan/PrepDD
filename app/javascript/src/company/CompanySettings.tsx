e841fa25b5d751c11ae2c9393bc4ff43f0199f88
khs125 <eden.net120@gmail.com>
2019-07-16 20:23:30 +0500

    Update company settings UI

--- app/javascript/src/company/CompanySettings.tsx
+++ app/javascript/src/company/CompanySettings.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Switch from '@material-ui/core/Switch';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -22,13 +24,25 @@ const useStyles = makeStyles(theme => ({
             color: theme.palette.text.secondary,
         },
     },
+    companySection: {
+        marginTop: 20,
+    },
+    table: {
+        minWidth: 650,
+        marginTop: 50,
+    },
 }));
+
+
 
 export default function CompanySettings(props: {path?: string}) {
     const classes = useStyles();
     const [state, setState] = React.useState({
-        Watermarking: false,
+        checkedA: false,
+        checkedB: true,
+        checkedC: false,
     });
+
     const handleChange = name => event => {
         setState({ ...state, [name]: event.target.checked });
     };
@@ -49,28 +63,165 @@ export default function CompanySettings(props: {path?: string}) {
                         <Typography variant="h6" gutterBottom>
                             PrepDD
                         </Typography>
+
                         <Divider/>
+
+                        <Grid className={classes.companySection} container spacing={3}>
+                            <Grid xs={6}>
+                                <Typography variant="body1" gutterBottom>
+                                    Parent Company
+                                </Typography>
+
+                                <Typography variant="h6" gutterBottom>
+                                    PrepDD
+                                </Typography>
+                                <Divider/>
+                            </Grid>
+
+                            <Grid xs={6}>
+                                <Typography variant="body1" gutterBottom>
+                                    Broker
+                                </Typography>
+
+                                <Typography variant="h6" gutterBottom>
+                                    PrepDD...
+                                </Typography>
+                                <Divider/>
+                            </Grid>
+                        </Grid>
+
+                        <Table className={classes.table}>
+                            <TableHead>
+                                <Typography variant="h5" gutterBottom>
+                                    Utilization Snapshot
+                                </Typography>
+                                <TableRow>
+                                    <TableCell>Product feature</TableCell>
+                                    <TableCell align="left">Current</TableCell>
+                                    <TableCell align="left">Max</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                    <TableRow key={'company-uti'}>
+                                        <TableCell component="th" scope="row">
+                                            <Typography variant="h6" gutterBottom>
+                                                Total users this month (2 days remain)
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <Typography variant="h6" gutterBottom>
+                                                5 Users
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <Typography variant="h6" gutterBottom>
+                                               7 Users
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+
+                                <TableRow key={'company-snap'}>
+                                        <TableCell component="th" scope="row">
+                                            <Typography variant="h6" gutterBottom>
+                                                Total storage used (GB)
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <Typography variant="h6" gutterBottom>
+                                                5.5 GB
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <Typography variant="h6" gutterBottom>
+                                               10GB
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                            </TableBody>
+                        </Table>
+
+
+                        <Table className={classes.table}>
+                            <TableHead>
+                                <Typography variant="h5" gutterBottom>
+                                    External File Sharing
+                                </Typography>
+
+                                <Typography variant="body2" gutterBottom>
+                                    Allow your users in your company to use these features when sharing files externally
+                                </Typography>
+                                <TableRow>
+                                    <TableCell></TableCell>
+                                    <TableCell align="right"></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow key={'company-uti'}>
+                                    <TableCell component="th" scope="row">
+                                        <Typography variant="h6" gutterBottom>
+                                            Total users this month (2 days remain)
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <Switch
+                                            checked={state.checkedA}
+                                            onChange={handleChange('checkedA')}
+                                            value="checkedA"
+                                            color="primary"
+                                            inputProps={{ 'aria-label': 'primary checkbox' }}
+                                        />
+                                    </TableCell>
+                                </TableRow>
+
+                                <TableRow key={'company-snap'}>
+                                    <TableCell component="th" scope="row">
+                                        <Typography variant="h6" gutterBottom>
+                                            Total storage used (GB)
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <Switch
+                                            checked={state.checkedB}
+                                            onChange={handleChange('checkedB')}
+                                            value="checkedB"
+                                            color="primary"
+                                            inputProps={{ 'aria-label': 'primary checkbox' }}
+                                        />
+                                    </TableCell>
+                                </TableRow>
+
+                                <TableRow key={'company-snap'}>
+                                    <TableCell component="th" scope="row">
+                                        <Typography variant="h6" gutterBottom>
+                                            Total storage used (GB)
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <Switch
+                                            checked={state.checkedC}
+                                            onChange={handleChange('checkedC')}
+                                            value="checkedC"
+                                            color="primary"
+                                            inputProps={{ 'aria-label': 'primary checkbox' }}
+                                        />
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
                     </Paper>
                 </Grid>
 
                 <Grid item xs={4}>
-                    <Paper className={classes.paper}>xs=12</Paper>
+                    <Paper className={classes.paper}>
+                        <TableCell component="th" scope="row">
+                            <Typography variant="h6" gutterBottom>
+                                Company Logo(WIP)
+                            </Typography>
+                        </TableCell>
+                    </Paper>
                 </Grid>
             </Grid>
 
-            <FormGroup row>
-                <FormControlLabel
-                    control={
-                        <Switch
-                            checked={state.Watermarking}
-                            onChange={handleChange('Watermarking')}
-                            value="Watermarking"
-                            color="primary"
-                        />
-                    }
-                    label="File PDF + Watermarking"
-                />
-            </FormGroup>
         </div>
     );
 }
