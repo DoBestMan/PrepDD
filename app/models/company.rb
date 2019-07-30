70093409b37ad2e2651e8b450962dffa231e487f
Aijaz Khan <aijaz-khan@hotmail.com>
2019-07-31 02:29:26 +0500

    Create S3 bucket on company creation

--- app/models/company.rb
+++ app/models/company.rb
@@ -9,6 +9,22 @@ class Company < ApplicationRecord
   validates :name, presence: true
 
   before_create :generate_encryption_key
+  after_create :create_s3
+
+  def create_s3
+    require 'aws-sdk-s3'
+    begin
+      s3 = Aws::S3::Client.new(region: 'us-west-2')
+      bucket = s3.create_bucket(bucket: "prepdd-#{self.name.downcase}")
+    rescue
+      errors.add(:name, :blank, message: "Not Able to create s3 bucket")
+    end
+
+    if bucket
+      self.s3_location = bucket.location
+      save!
+    end
+  end
 
   def generate_encryption_key
     self.encryption_key = SecureRandom.urlsafe_base64(256)
