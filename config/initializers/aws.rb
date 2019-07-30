86849cdbe079faac0d46f29e0ab9c7a549d11bf1
Aijaz Khan <aijaz-khan@hotmail.com>
2019-07-31 02:32:51 +0500

    Update aws config file

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,1 +1,5 @@
-#Aws::S3::Resource.new(region: 'us-east-1')
+require 'aws-sdk'
+Aws.config.update(
+  credentials: Aws::Credentials.new(ENV['ACCESS_KEY_ID'], ENV['SECRET_ACCESS_KEY']),
+  region: 'us-east-1',
+  )
