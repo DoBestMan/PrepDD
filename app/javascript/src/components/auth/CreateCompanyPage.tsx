31c4650deed7a183013ce80aa2e30ee245999116
Aijaz Khan <aijaz-khan@hotmail.com>
2019-07-31 16:55:47 +0500

    Update Companay model validations

--- app/javascript/src/components/auth/CreateCompanyPage.tsx
+++ app/javascript/src/components/auth/CreateCompanyPage.tsx
@@ -1,11 +1,16 @@
-import React, {useCallback, useState} from 'react';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import FlashMessage from '../common/FlashMessage';
+import Grid from '@material-ui/core/Grid';
+import idx from 'idx';
+import React, {useCallback, useEffect, useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import {navigate} from '@reach/router';
 import {makeStyles} from '@material-ui/core/styles';
 import {useRequireGuest} from '../../hooks/auth';
+import {Link as RouterLink, navigate} from '@reach/router';
+
+import {useCompanyCreate} from "../../graphql/mutations/CreateCompany";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -24,63 +29,110 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  companyTitle: {
+    textAlign: 'center',
+  }
 }));
 
 export default function CreateCompanyPage(_props: {path?: string}) {
-  useRequireGuest();
 
   const classes = useStyles({});
 
   const [state, setState] = useState<{
-    companyName: string;
+    name: string
   }>({
-    companyName: '',
+    name: ''
   });
+
+
+  const [createCompany, {data}] = useCompanyCreate({
+    name: state.name,
+  });
+
+  useEffect(() => {
+    if (idx(data, data => data.createCompany.success)) {
+      localStorage.setItem('CompanyName', state.name);
+      navigate('/signup');
+    }
+  }, [data]);
+
+  function errorFor(path: string) {
+    const errors = idx(data, x => x.createCompany.errors);
+    if (!errors) {
+      return;
+    }
+    const error = errors.find((e: {path: string}) => e.path === path);
+    if (!error) {
+      return;
+    }
+    return error.message;
+  }
 
   const onSubmit = useCallback(
     e => {
-      console.log('called...');
       e.preventDefault();
-      navigate('/signup');
+      createCompany();
     },
-    [],
+    [createCompany]
   );
 
   const onChangeInput = useCallback(
     e => {
       const {name, value} = e.target;
       setState(state => ({...state, [name]: value}));
-      localStorage.setItem('CompanyName', value);
     },
     [setState]
   );
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="lg">
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          New Company
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={onSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            label="Company Name"
-            name="companyName"
-            onChange={onChangeInput}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Continue
-          </Button>
-        </form>
+        <Grid container spacing={5}>
+          <Grid item xs={3}>
+          </Grid>
+          <Grid item xs={5}>
+            <div className={classes.companyTitle}>
+              <Typography component="h1" variant="h5">
+                Create New Company
+              </Typography>
+            </div>
+            <form className={classes.form} noValidate onSubmit={onSubmit}>
+              {errorFor('root') && (
+                <FlashMessage
+                  className={classes.flash}
+                  variant="warning"
+                  message={errorFor('root')}
+                />
+              )}
+
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Company Name"
+                name="name"
+                autoFocus
+                value={state.name}
+                error={!!errorFor('name')}
+                helperText={errorFor('name')}
+                onChange={onChangeInput}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Continue
+              </Button>
+            </form>
+
+
+          </Grid>
+        </Grid>
       </div>
     </Container>
   );
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,11 +2,12 @@ class Company < ApplicationRecord
 
   belongs_to :parent, class_name: 'Company', optional: true
   has_many :children, class_name: 'Company', foreign_key: 'parent_id'
-  belongs_to :owner, class_name: 'User'
+  belongs_to :owner, class_name: 'User', optional: true
   has_many :employees, class_name: 'User', dependent: :destroy
   belongs_to :subscription, optional: true
 
   validates :name, presence: true
+  validates :name, uniqueness: true
 
   before_create :generate_encryption_key
   after_create :create_s3, :create_kms
