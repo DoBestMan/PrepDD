b8555ad324a416300c0bff4445b711708a53b988
Aijaz Khan <aijaz-khan@hotmail.com>
2019-07-31 16:50:58 +0500

    Create createCompany BE mutaion

---
+++ app/graphql/mutations/create_company.rb
@@ -0,0 +1,33 @@
+class Mutations::CreateCompany < GraphQL::Schema::Mutation
+  argument :name, String, required: true
+
+  field :company, Types::CompanyType, null: true
+  field :errors, [Types::FormErrorType], null: false
+  field :success, Boolean, null: false
+
+  def resolve(name: nil)
+    response = { errors: [] }
+
+    if context[:controller].user_signed_in?
+      company = Company.create({ name: name, owner_id: context[:controller].current_user.id })
+    else
+      company = Company.create({ name: name })
+    end
+    
+    company.errors.messages.each do |path, messages|
+      messages.each do |message|
+        response[:errors].push(
+          { path: path.to_s.camelcase(:lower), message: message }
+        )
+        response[:success] = false
+      end
+    end
+
+    if company&.persisted?
+      response[:success] = true
+      response
+    end
+
+    response
+  end
+end
