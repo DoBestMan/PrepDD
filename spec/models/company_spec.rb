ea9ce4f84588602dcab3504aefb1e2293bf8634c
Aijaz Khan <aijaz-khan@hotmail.com>
2019-07-31 23:33:22 +0500

    Add company association BE specs

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe Company, type: :model do
   let!(:company) { create(:company) }
 
-  describe Company, 'validations' do
+  describe '#validations' do
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:name) }
+  end
+
+  describe '#associations' do
+    it { should have_many :employees }
+    it { should belong_to :owner }
+    it { should belong_to :subscription }
   end
 
   describe '#callback' do
