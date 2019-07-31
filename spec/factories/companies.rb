5ee3a4900924bbdfbd663ac118547174228deb15
Aijaz Khan <aijaz-khan@hotmail.com>
2019-07-31 22:36:23 +0500

    Add shoulda-matchers config for BE tests

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :company do
-    sequence(:full_name) { Faker::Company.name }
+    sequence(:name) { Faker::Company.name }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,25 +8,8 @@ if Rails.env.production?
 end
 require 'rspec/rails'
 require 'support/factory_bot'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'shoulda/matchers'
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -62,4 +45,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #
+  # Configures shoulda-matchers
+
+  # @see https://github.com/thoughtbot/shoulda-matchers#configuration
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
