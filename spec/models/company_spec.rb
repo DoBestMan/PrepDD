35e8a9ffd8371816e18e75a7228cee30d6938816
Aijaz Khan <aijaz-khan@hotmail.com>
2019-07-31 22:52:16 +0500

    Add BE specs for s3 encryption_key & kms creation

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
+  let!(:company) { create(:company) }
+
   describe Company, 'validations' do
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:name) }
+  end
+
+  describe '#callback' do
+    # Company callback
+    describe '#method' do
+      it { is_expected.to callback(:create_s3).after(:create) }
+      it { is_expected.to callback(:create_kms).after(:create) }
+      it { is_expected.to callback(:generate_encryption_key).before(:create) }
+    end
   end
 end
