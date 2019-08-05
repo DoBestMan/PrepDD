2a97c1ffaec565abc2c2a7dffc2b39118a172a24
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-05 18:57:51 +0500

    Add unit test for company creation

---
+++ app/javascript/src/__tests__/auth/companyCreatePage.test.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import CreateCompanyPage from '../../components/auth/CreateCompanyPage';
+
+test('Create new Company text', () => {
+  const createComapny = shallow(<CreateCompanyPage />);
+  expect(createComapny.text()).toContain('Create New Company');
+});
+
--- app/javascript/src/__tests__/setupTests.js
+++ app/javascript/src/__tests__/setupTests.js
@@ -1,4 +1,4 @@
-import { configure } from "enzyme";
-import React16Adapter from "enzyme-adapter-react-16";
+import { configure } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new React16Adapter() });
+configure({ adapter: new EnzymeAdapter() });
