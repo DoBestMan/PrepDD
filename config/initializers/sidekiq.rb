8c617ed0516715cea61413be1f26999ef7f26b57
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-06 18:57:36 +0500

    Update redis url for staging

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://h:p1da991059a1dfa30351ed23a72902fbdc6f3fd18db44cf18245c601c550c1000@ec2-3-222-82-60.compute-1.amazonaws.com:27059' }
+  config.redis = { url: 'redis://h:p67ce11c00bdb54a0f694ea7f74e538a4b56a28db995ad378e1eb761dd266c41f@ec2-3-222-121-40.compute-1.amazonaws.com:19469' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://h:p1da991059a1dfa30351ed23a72902fbdc6f3fd18db44cf18245c601c550c1000@ec2-3-222-82-60.compute-1.amazonaws.com:27059' }
+  config.redis = { url: 'redis://h:p67ce11c00bdb54a0f694ea7f74e538a4b56a28db995ad378e1eb761dd266c41f@ec2-3-222-121-40.compute-1.amazonaws.com:19469' }
 end
