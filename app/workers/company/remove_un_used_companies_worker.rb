bdafcb8cd9730aec8e4cb884c1ceaabbb8c995c3
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-09 02:27:44 +0500

    Add worker to remove un-used companies

--- app/workers/company/remove_un_used_companies_worker.rb
+++ app/workers/company/remove_un_used_companies_worker.rb
@@ -1,7 +1,12 @@
 class Company::RemoveUnUsedCompaniesWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform
+    companies = Company.where(owner_id: nil)
+    companies.each do |company|
+      if company.created_at < 1.day.ago
+        company.destroy
+      end
+    end
   end
 end
