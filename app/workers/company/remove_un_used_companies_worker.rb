3d94f9a690b02fc817f446e401b770a22cc5e393
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-09 13:35:38 +0500

    Remove s3 bucket on deleting company

--- app/workers/company/remove_un_used_companies_worker.rb
+++ app/workers/company/remove_un_used_companies_worker.rb
@@ -5,6 +5,14 @@ class Company::RemoveUnUsedCompaniesWorker
     companies = Company.where(owner_id: nil)
     companies.each do |company|
       if company.created_at < 1.day.ago
+
+        # Remove s3 bucket of company
+        s3_client = Aws::S3::Client.new
+        bucket_name = company.s3_location.gsub('/', '')
+        s3_client.delete_bucket(bucket: bucket_name)
+
+        # Remove KMS key of company
+        
         company.destroy
       end
     end
