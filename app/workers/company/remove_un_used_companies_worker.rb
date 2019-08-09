1860f2a9f3c13209b3e2d00355e6bdb77cd6eb6a
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-10 00:26:18 +0500

    Add rescue block to avoid exceptions

--- app/workers/company/remove_un_used_companies_worker.rb
+++ app/workers/company/remove_un_used_companies_worker.rb
@@ -5,18 +5,21 @@ class Company::RemoveUnUsedCompaniesWorker
     Company.where(owner_id: nil).each do |company|
       if company.created_at < 1.day.ago
 
-        # Remove s3 bucket of company
-        if company.s3_location?
-          s3_client = Aws::S3::Client.new
-          bucket_name = company.s3_location.gsub('/', '')
-          s3_client.delete_bucket(bucket: bucket_name)
-        end
+        begin
+          # Remove s3 bucket of company
+          if company.s3_location?
+            s3_client = Aws::S3::Client.new
+            bucket_name = company.s3_location.gsub('/', '')
+            s3_client.delete_bucket(bucket: bucket_name)
+          end
 
-        # Remove KMS key of company
-        if company.kms_key_id?
-          client = Aws::KMS::Client.new
-          key_id = company.kms_key_id
-          resp = client.schedule_key_deletion({ key_id: key_id, pending_window_in_days: 7 })
+          # Remove KMS key of company
+          if company.kms_key_id?
+            client = Aws::KMS::Client.new
+            key_id = company.kms_key_id
+            resp = client.schedule_key_deletion({ key_id: key_id, pending_window_in_days: 7 })
+          end
+        rescue
         end
 
         company.destroy
