622ff0ebbe369bb5caf219e135d56484c406c8f1
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-11 12:11:47 +0500

    Update user profile updation mutations

--- app/graphql/mutations/update_user_details.rb
+++ app/graphql/mutations/update_user_details.rb
@@ -3,21 +3,33 @@ module Mutations
     argument :fullName, String, required: true
     argument :email, String, required: true
     argument :companyName, String, required: true
+    argument :oldPassword, String, required: true
+    argument :newPassword, String, required: true
 
     field :user, Types::UserType, null: true
     field :errors, [Types::FormErrorType], null: false
     field :success, Boolean, null: false
 
-    def resolve(full_name: nil, email: nil, company_name: nil)
+    def resolve(full_name: nil, email: nil, company_name: nil, old_password: nil, new_password: nil)
       response = { errors: [] }
+
+      user = User.find_for_database_authentication(email: email)
+
+      unless user && user.valid_password?(password)
+        response[:errors].push(
+          { path: 'root', message: 'Incorrect email or password.' }
+        )
+        response[:success] = false
+        return response
+      end
 
       user =
         User.update(
           {
             full_name: full_name,
             email: email,
-            password: password,
-            password_confirmation: password
+            password: new_password,
+            password_confirmation: new_password
           }
         )
 
