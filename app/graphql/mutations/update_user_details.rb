b5cce9d3a554fd4efdbaaad510af23cbacba5246
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-11 14:51:43 +0500

    Add user signed in validation on updating user info

--- app/graphql/mutations/update_user_details.rb
+++ app/graphql/mutations/update_user_details.rb
@@ -2,16 +2,19 @@ module Mutations
   class UpdateUserDetails < GraphQL::Schema::RelayClassicMutation
     argument :fullName, String, required: true
     argument :email, String, required: true
-    argument :companyName, String, required: true
-    argument :oldPassword, String, required: true
-    argument :newPassword, String, required: true
 
     field :user, Types::UserType, null: true
     field :errors, [Types::FormErrorType], null: false
     field :success, Boolean, null: false
 
-    def resolve(full_name: nil, email: nil, company_name: nil, old_password: nil, new_password: nil)
+    def resolve(full_name: nil, email: nil)
       response = { errors: [] }
+
+      if !context[:controller].user_signed_in?
+        response[:errors].push({ path: 'root', message: 'Not authorized to do it' })
+        response[:success] = false
+        return response
+      end
 
       user = User.find_for_database_authentication(email: email)
 
