27f2e26249537020d55065d4d64e650fc1798eea
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-14 23:03:26 +0500

    Remove old password field from User Update password mutation

--- app/graphql/mutations/update_user_password.rb
+++ app/graphql/mutations/update_user_password.rb
@@ -1,14 +1,12 @@
 module Mutations
   class UpdateUserPassword < GraphQL::Schema::RelayClassicMutation
-    argument :email, String, required: true
-    argument :oldPassword, String, required: true
-    argument :newPassword, String, required: true
+    argument :password, String, required: true
 
     field :user, Types::UserType, null: true
     field :errors, [Types::FormErrorType], null: false
     field :success, Boolean, null: false
 
-    def resolve(old_password: nil, new_password: nil, email: nil)
+    def resolve(password: nil)
       response = { errors: [] }
 
       if !context[:controller].user_signed_in?
@@ -17,15 +15,7 @@ module Mutations
         return response
       end
 
-      user = User.find_for_database_authentication(email: email)
-
-      unless user && user.valid_password?(old_password)
-        response[:errors].push(
-          { path: 'root', message: 'Incorrect email or password.' }
-        )
-        response[:success] = false
-        return response
-      end
+      user = context[:controller].current_user
 
       user = User.
         update({
