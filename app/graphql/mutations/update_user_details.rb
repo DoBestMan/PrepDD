25f06fd4abbeceb71681c05d1a5ae87c62472803
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-14 18:52:28 +0500

    Update back-end mutations

--- app/graphql/mutations/update_user_details.rb
+++ app/graphql/mutations/update_user_details.rb
@@ -1,56 +1,55 @@
-module Mutations
-  class UpdateUserDetails < GraphQL::Schema::RelayClassicMutation
-    argument :fullName, String, required: true
-    argument :email, String, required: true
+class Mutations::UpdateUserDetails < GraphQL::Schema::Mutation
+  argument :fullName, String, required: true
+  argument :email, String, required: true
+  argument :displayName, String, required: true
 
-    field :user, Types::UserType, null: true
-    field :errors, [Types::FormErrorType], null: false
-    field :success, Boolean, null: false
+  field :user, Types::UserType, null: true
+  field :errors, [Types::FormErrorType], null: false
+  field :success, Boolean, null: false
 
-    def resolve(full_name: nil, email: nil)
-      response = { errors: [] }
+  def resolve(full_name: nil, email: nil)
+    response = { errors: [] }
 
-      if !context[:controller].user_signed_in?
-        response[:errors].push({ path: 'root', message: 'Not authorized to do it' })
-        response[:success] = false
-        return response
-      end
-
-      user = User.find_for_database_authentication(email: email)
-
-      unless user && user.valid_password?(password)
-        response[:errors].push(
-          { path: 'root', message: 'Incorrect email or password.' }
-        )
-        response[:success] = false
-        return response
-      end
-
-      user =
-        User.update(
-          {
-            full_name: full_name,
-            email: email,
-            password: new_password,
-            password_confirmation: new_password
-          }
-        )
-
-      user.errors.messages.each do |path, messages|
-        messages.each do |message|
-          response[:errors].push(
-            { path: path.to_s.camelcase(:lower), message: message }
-          )
-          response[:success] = false
-        end
-      end
-
-      if user.persisted?
-        response[:user] = user
-        response[:success] = true
-      end
-
-      response
+    if !context[:controller].user_signed_in?
+      response[:errors].push({ path: 'root', message: 'Not authorized to do it' })
+      response[:success] = false
+      return response
     end
+
+    user = User.find_for_database_authentication(email: email)
+
+    unless user && user.valid_password?(password)
+      response[:errors].push(
+        { path: 'root', message: 'Incorrect email or password.' }
+      )
+      response[:success] = false
+      return response
+    end
+
+    user =
+      User.update(
+        {
+          full_name: full_name,
+          email: email,
+          password: new_password,
+          password_confirmation: new_password
+        }
+      )
+
+    user.errors.messages.each do |path, messages|
+      messages.each do |message|
+        response[:errors].push(
+          { path: path.to_s.camelcase(:lower), message: message }
+        )
+        response[:success] = false
+      end
+    end
+
+    if user.persisted?
+      response[:user] = user
+      response[:success] = true
+    end
+
+    response
   end
 end
--- app/graphql/mutations/update_user_password.rb
+++ app/graphql/mutations/update_user_password.rb
@@ -1,53 +1,51 @@
-module Mutations
-  class UpdateUserPassword < GraphQL::Schema::RelayClassicMutation
-    argument :email, String, required: true
-    argument :oldPassword, String, required: true
-    argument :newPassword, String, required: true
+class Mutations::UpdateUserPassword < GraphQL::Schema::Mutation
+  argument :email, String, required: true
+  argument :oldPassword, String, required: true
+  argument :newPassword, String, required: true
 
-    field :user, Types::UserType, null: true
-    field :errors, [Types::FormErrorType], null: false
-    field :success, Boolean, null: false
+  field :user, Types::UserType, null: true
+  field :errors, [Types::FormErrorType], null: false
+  field :success, Boolean, null: false
 
-    def resolve(old_password: nil, new_password: nil, email: nil)
-      response = { errors: [] }
+  def resolve(old_password: nil, new_password: nil, email: nil)
+    response = { errors: [] }
 
-      if !context[:controller].user_signed_in?
-        response[:errors].push({ path: 'root', message: 'Not authorized to do it' })
-        response[:success] = false
-        return response
-      end
+    if !context[:controller].user_signed_in?
+      response[:errors].push({ path: 'root', message: 'Not authorized to do it' })
+      response[:success] = false
+      return response
+    end
 
-      user = User.find_for_database_authentication(email: email)
+    user = User.find_for_database_authentication(email: email)
 
-      unless user && user.valid_password?(old_password)
+    unless user && user.valid_password?(old_password)
+      response[:errors].push(
+        { path: 'root', message: 'Incorrect email or password.' }
+      )
+      response[:success] = false
+      return response
+    end
+
+    user = User.
+      update({
+               password: password,
+               password_confirmation: password
+             })
+
+    user.errors.messages.each do |path, messages|
+      messages.each do |message|
         response[:errors].push(
-          { path: 'root', message: 'Incorrect email or password.' }
+          { path: path.to_s.camelcase(:lower), message: message }
         )
         response[:success] = false
-        return response
       end
-
-      user = User.
-        update({
-                 password: password,
-                 password_confirmation: password
-               })
-
-      user.errors.messages.each do |path, messages|
-        messages.each do |message|
-          response[:errors].push(
-            { path: path.to_s.camelcase(:lower), message: message }
-          )
-          response[:success] = false
-        end
-      end
-
-      if user.persisted?
-        response[:user] = user
-        response[:success] = true
-      end
-
-      response
     end
+
+    if user.persisted?
+      response[:user] = user
+      response[:success] = true
+    end
+
+    response
   end
 end
