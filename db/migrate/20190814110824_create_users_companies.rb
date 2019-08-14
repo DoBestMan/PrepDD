551ade1a1656131af71c6c6b4849c1f69ed24f6e
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-14 16:09:14 +0500

    Update schema file

--- db/migrate/20190814110824_create_users_companies.rb
+++ db/migrate/20190814110824_create_users_companies.rb
@@ -3,7 +3,6 @@ class CreateUsersCompanies < ActiveRecord::Migration[5.2]
     create_table :users_companies do |t|
       t.belongs_to :company
       t.belongs_to :user
-
       t.timestamps
     end
   end
