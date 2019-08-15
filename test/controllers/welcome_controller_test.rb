0cad163edbd146e10b30b1a337549b004b92167d
Andy Niccolai <andy.niccolai@gmail.com>
2019-08-14 19:44:53 -0700

    Fix/heroku test env (#45)

--- test/controllers/welcome_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  # test 'should get get' do
-  #   get welcome_get_url
-  #   assert_response :success
-  # end
-end
