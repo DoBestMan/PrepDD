5ed565d18fc6dbdb2f5d0889904708ace8cee26d
Andy Niccolai <andy.niccolai@gmail.com>
2019-08-14 18:53:10 -0700

    Add app/assets to webpacker config, fix a few of the incorrect urls

--- app/javascript/src/modules/layout/components/SideBar/SideBar.tsx
+++ app/javascript/src/modules/layout/components/SideBar/SideBar.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import clsx from 'clsx';
-import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
+import {Theme, makeStyles, createStyles} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import AddIcon from '@material-ui/icons/Add'
+import AddIcon from '@material-ui/icons/Add';
 
-import { MainListItems, AdminListItems } from './components/NavItems'
+import {MainListItems, AdminListItems} from './components/NavItems';
 import StyledButton from './components/StyledButton';
+
+const PrepddLogo = require('images/logos/prepdd-logo.svg');
 
 const drawerWidth = 210;
 
-const useStyles = makeStyles((theme: Theme) => 
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     mark: {
       fontFamily: 'Montserrat',
       fontSize: '18px',
-      fontWeight: 'bold', 
+      fontWeight: 'bold',
       color: '#0E3B5C',
-      lineHeight: '48px'
+      lineHeight: '48px',
     },
     primaryColor: {
-      color: '#3A84FF'
+      color: '#3A84FF',
     },
     drawerPaper: {
-      boxSizing: 'border-box', 
+      boxSizing: 'border-box',
       width: drawerWidth,
       background: '#FFFFFF',
       border: '1px solid #D8D8D8',
@@ -37,7 +39,7 @@ const useStyles = makeStyles((theme: Theme) =>
       }),
     },
     drawerPaperClose: {
-      boxSizing: 'border-box', 
+      boxSizing: 'border-box',
       transition: theme.transitions.create('width', {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
@@ -50,17 +52,17 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     paddingOpen: {
       paddingLeft: 24,
-      paddingRight: 24
+      paddingRight: 24,
     },
     paddingClose: {
-      paddingLeft: 18, 
-      paddingRight: 18
+      paddingLeft: 18,
+      paddingRight: 18,
     },
     marginOpen: {
-      margin: '15px 24px 15px 24px'
+      margin: '15px 24px 15px 24px',
     },
     marginClose: {
-      margin: '15px 18px 15px 18px'
+      margin: '15px 18px 15px 18px',
     },
   })
 );
@@ -71,7 +73,7 @@ interface SideBarProps {
 }
 
 export default function SideBar(props: SideBarProps) {
-  const { open, setShowNarrow } = props;
+  const {open, setShowNarrow} = props;
   const classes = useStyles();
 
   return (
@@ -85,9 +87,12 @@ export default function SideBar(props: SideBarProps) {
       onMouseOut={() => setShowNarrow(false)}
     >
       <List disablePadding>
-        <ListItem className={clsx(classes.paddingOpen, !open && classes.paddingClose)} disableGutters>
+        <ListItem
+          className={clsx(classes.paddingOpen, !open && classes.paddingClose)}
+          disableGutters
+        >
           <ListItemIcon>
-            <img src="../assets/img/logos/prepdd-logo.svg" alt="PrepDD"/>
+            <img src={PrepddLogo} alt="PrepDD" />
           </ListItemIcon>
           <ListItemText>
             <span className={classes.mark}>
@@ -95,17 +100,25 @@ export default function SideBar(props: SideBarProps) {
             </span>
           </ListItemText>
         </ListItem>
-        <ListItem className={clsx(classes.paddingOpen, !open && classes.paddingClose)} disableGutters>
-          {open ?
-            <StyledButton variant="outlined" color="primary">Create</StyledButton> :
+        <ListItem
+          className={clsx(classes.paddingOpen, !open && classes.paddingClose)}
+          disableGutters
+        >
+          {open ? (
+            <StyledButton variant="outlined" color="primary">
+              Create
+            </StyledButton>
+          ) : (
             <StyledButton variant="outlined" color="primary">
               <AddIcon />
             </StyledButton>
-          }
-        </ListItem>          
+          )}
+        </ListItem>
         <MainListItems open={open} />
       </List>
-      <Divider className={clsx(classes.marginOpen, !open && classes.marginClose)}/>
+      <Divider
+        className={clsx(classes.marginOpen, !open && classes.marginClose)}
+      />
       <List disablePadding>
         <AdminListItems open={open} />
       </List>
