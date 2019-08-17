4af360a5fb60b4e21b5f6713934125ccb509ca28
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-17 17:44:59 +0500

    Include subscription name in type

--- app/graphql/types/subscription_type.rb
+++ app/graphql/types/subscription_type.rb
@@ -1,6 +1,7 @@
 module Types
   class SubscriptionType < GraphQL::Schema::Object
   field :id, ID, null: false
+  field :name, String, null: false
   field :description, String, null: true
   field :max_users, String, null: true
   field :max_storage, String, null: true
