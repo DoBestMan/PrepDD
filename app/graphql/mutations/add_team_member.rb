9e3b21afc7d209f926db91818ef338d5444cc60b
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-17 20:25:31 +0500

    Update team mutation fields

--- app/graphql/mutations/add_team_member.rb
+++ app/graphql/mutations/add_team_member.rb
@@ -1,19 +1,44 @@
 class Mutations::AddTeamMember <  GraphQL::Schema::Mutation
   argument :teamId, ID, required: true
-  argument :userId, ID, required: true
+  argument :email, String, required: true
+  argument :fullName, String, required: true
+  argument :role, String, required: true
 
   field :team, Types::TeamType, null: true
   field :errors, [Types::FormErrorType], null: false
   field :success, Boolean, null: false
 
-  def resolve(team_id: nil, user_id: nil)
+  def resolve(team_id: nil, email: nil, full_name: nil, role: nil)
     response = { errors: [] }
 
-    team_user = TeamsUser.create(
-                           user_id: user_id, team_id: team_id
-    )
+    user = User.find_by_email(email)
+
+    if !user.present?
+      password = Devise.friendly_token[0,20]
+      user =
+        User.create(
+          {
+            full_name: full_name,
+            email: email,
+            password: password,
+            password_confirmation: password,
+          }
+        )
+    end
+
+    team_user = TeamsUser.create( user_id: user.id, team_id: team_id )
+
 
     team_user.errors.messages.each do |path, messages|
+      messages.each do |message|
+        response[:errors].push(
+          { path: path.to_s.camelcase(:lower), message: message }
+        )
+        response[:success] = false
+      end
+      end
+
+    user.errors.messages.each do |path, messages|
       messages.each do |message|
         response[:errors].push(
           { path: path.to_s.camelcase(:lower), message: message }
