f276b061648640ef57fa52ef4b09e146fa65b911
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-17 17:26:44 +0500

    Add title to subscription

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_111651) do
+ActiveRecord::Schema.define(version: 2019_08_17_122452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_111651) do
     t.boolean "modify_subscription"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "teams", force: :cascade do |t|
