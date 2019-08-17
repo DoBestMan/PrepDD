2f8de8d056d2a195ceebef0425560bfb1b2d6344
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-17 16:44:04 +0500

    Update Company type to add settings

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -1,13 +1,16 @@
 module Types
   class CompanyType < GraphQL::Schema::Object
     field :id, ID, null: false
-    field :parent_id, ID, null: false
-    field :broker_co_id, ID, null: false
-    field :subscription_id, ID, null: false
-    field :owner_id, ID, null: false
-    field :is_active, Boolean, null: false
     field :name, String, null: false
+    field :is_active, Boolean, null: true
+    field :auto_pdf, Boolean, null: true
+    field :auto_watermark, Boolean, null: true
+    field :preview_only, Boolean, null: true
+    field :subscription, SubscriptionType, null: true
+
     field :owner, UserType, null: false
+    field :parent, CompanyType, null: true
+
     field :users, [UserType], null: false
     field :teams, [TeamType], null: false
   end
