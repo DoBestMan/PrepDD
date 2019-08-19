8de93587d7cec14f284a0488cb4c64f4677e9e92
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-19 20:19:54 +0500

    Remove role with user

--- app/graphql/mutations/remove_company_member.rb
+++ app/graphql/mutations/remove_company_member.rb
@@ -13,7 +13,8 @@ class Mutations::RemoveCompanyMember < GraphQL::Schema::Mutation
     if user_id
       user = User.find(user_id)
       user.teams.where(company_id: company_id).destroy_all
-      user.companies.where(id: company_id).first.destroy!
+      user.companies.where(id: company_id).first&.destroy!
+      RolesUser.where(user_id: user_id, company_id: company_id).first&.destroy!
 
     elsif user_ids.present?
       user_ids.each do |id|
