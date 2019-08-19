2cf0856521afc79d3e01bc4cc0c5613067c5ac5c
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-19 17:08:47 +0500

    Remove multiple users from teams

--- app/graphql/mutations/remove_team_member.rb
+++ app/graphql/mutations/remove_team_member.rb
@@ -1,23 +1,32 @@
 class Mutations::RemoveTeamMember < GraphQL::Schema::Mutation
   argument :teamId, ID, required: true
-  argument :userId, ID, required: true
+  argument :userId, ID, required: false
+  argument :userIds, [ID], required: false
 
   field :team, Types::TeamType, null: true
   field :errors, [Types::FormErrorType], null: false
   field :success, Boolean, null: false
 
-  def resolve(team_id: nil, user_id: nil)
+  def resolve(team_id: nil, user_id: nil, user_ids: nil)
     response = { errors: [] }
 
-    team_user = TeamsUser.where(user_id: user_id, team_id: team_id).first
+    if user_id
+      team_user = TeamsUser.where(user_id: user_id, team_id: team_id).first
+      if team_user
+        team_user.destroy!
+      else
+        response[:errors].push({ path: 'root', message: 'No Team found for this user.' })
+        response[:success] = false
+        return response
+      end
 
-
-    if team_user
-      team_user.destroy!
-    else
-      response[:errors].push({ path: 'root', message: 'No Team found for this user.' })
-      response[:success] = false
-      return response
+    elsif user_ids.present?
+      user_ids.each do |id|
+        team_user = TeamsUser.where(user_id: id, team_id: team_id).first
+        if team_user
+          team_user.destroy!
+        end
+      end
     end
 
     team_user.errors.messages.each do |path, messages|
@@ -28,7 +37,6 @@ class Mutations::RemoveTeamMember < GraphQL::Schema::Mutation
         response[:success] = false
       end
     end
-
 
     response[:success] = true
     response
