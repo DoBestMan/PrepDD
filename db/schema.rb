7e111e960447d82ad5dc697ee7cedcd7d03ed5a5
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-24 21:32:30 +0500

    Add parent company model & table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_161311) do
+ActiveRecord::Schema.define(version: 2019_08_24_162739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 2019_08_24_161311) do
   end
 
   create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
+  end
+
+  create_table "parent_companies", force: :cascade do |t|
+    t.bigint "child_company_id"
+    t.bigint "parent_company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["child_company_id"], name: "index_parent_companies_on_child_company_id"
+    t.index ["parent_company_id"], name: "index_parent_companies_on_parent_company_id"
   end
 
   create_table "roles", force: :cascade do |t|
