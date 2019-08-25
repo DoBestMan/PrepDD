e3221849af378337e13216ca1192e2ffd61092b3
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-25 23:32:24 +0500

    Add delete parent and broker company mutation

--- app/graphql/mutations/update_company_settings.rb
+++ app/graphql/mutations/update_company_settings.rb
@@ -7,34 +7,46 @@ class Mutations::UpdateCompanySettings < GraphQL::Schema::Mutation
   argument :dynamicWatermarking, Boolean, required: false
   argument :previewOnly, Boolean, required: false
 
+  argument :deleteParentId, ID, required: false
+  argument :deleteBrokerId, ID, required: false
+
   field :company, Types::CompanyType, null: false
   field :errors, [Types::FormErrorType], null: false
   field :success, Boolean, null: false
 
   def resolve(id: nil, name: nil, parent_id: nil, broker_id: nil, automatic_pdf: nil,
-              dynamic_watermarking: nil, preview_only: nil)
+              dynamic_watermarking: nil, preview_only: nil, delete_broker_id: nil,
+              delete_parent_id: nil)
 
     response = { errors: [] }
     company = Company.find(id)
 
     company&.update(
-             name: name,
-             automatic_pdf: automatic_pdf,
-             preview_only: preview_only,
-             dynamic_watermarking: dynamic_watermarking
+      name: name,
+      automatic_pdf: automatic_pdf,
+      preview_only: preview_only,
+      dynamic_watermarking: dynamic_watermarking
     )
 
     if parent_id.present?
-      ParentCompany(child_company_id: company&.id, parent_company_id: parent_id)
+      ParentCompany.create(child_company_id: company&.id, parent_company_id: parent_id)
     end
 
     if broker_id.present?
       BrokerCompany.create(child_broker_id: company&.id, parent_broker_id: broker_id)
     end
-    
+
+    if delete_parent_id.present?
+      ParentCompany.where(child_company_id: company&.id, parent_company_id: delete_parent_id).first&.destroy!
+    end
+
+    if delete_broker_id.present?
+      BrokerCompany.where(child_broker_id: company&.id, parent_broker_id: delete_broker_id).first&.destroy!
+    end
+
     if company&.persisted?
       response[:success] = true
-      response[:companies] = company
+      response[:company] = company
       response
     end
 
