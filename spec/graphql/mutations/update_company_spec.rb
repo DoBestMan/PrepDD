410c5c099dded2f13289e623dcae9035cf933107
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-27 20:18:35 +0500

    Add update company mutations

---
+++ spec/graphql/mutations/update_company_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+module Mutations
+
+  RSpec.describe UpdateCompany, type: :request do
+    describe '.resolve' do
+      it 'update a company details' do
+        company = create(:company)
+        expect do
+          post '/graphql', params: { query: query(name: company.name) }
+          post '/graphql', params: { query: query_update_company(id: company.id, name: 'PrepDD-Test') }
+          it { expect(Company.first.full_name).to eq 'PrepDD-Test' }
+        end
+      end
+    end
+
+    def query(name:)
+      <<~GQL
+       mutation{
+    CreateCompany(
+      name: #{name}, 
+    ) {
+      errors {
+        path
+        message
+      }
+      success
+    }
+  }
+      GQL
+    end
+
+    def query_update_company(id:, name:)
+      <<~GQL
+       mutation{
+      updateCompany(
+      id: #{id}, 
+      name: #{name}
+    ) {
+      errors {
+        path
+        message
+      }
+      success
+    }
+  }
+      GQL
+    end
+
+  end
+end
