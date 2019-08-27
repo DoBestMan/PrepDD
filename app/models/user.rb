2dac977acd1a2c2fb1bccfba42a7b9b07700a4fe
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-28 00:11:53 +0500

    Remove extra space

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   has_one_attached :profile_picture
 
   def self.search(query)
-    User.where("lower(email) LIKE :text OR lower(full_name) LIKE :text", text:  "%#{query.downcase}%")
+    User.where("lower(email) LIKE :text OR lower(full_name) LIKE :text", text: "%#{query.downcase}%")
   end
 
   def email_or_uuid
