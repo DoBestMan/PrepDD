9ad2af3661026204e7993bf7dfc85019d5860aae
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-30 23:52:51 +0500

    Add Company and lists associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ApplicationRecord
-
+  belongs_to :requester, class_name: 'Company', foreign_key: 'requester_id'
+  belongs_to :responder, class_name: 'Company', foreign_key: 'responder_id', optional: true
 end
