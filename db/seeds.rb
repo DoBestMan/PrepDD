2a448d9619a9a53ac7935ca73377d5e734333054
Mark Franciose <mark.franciose@gmail.com>
2019-08-30 00:19:09 -0500

    add simple seeds to help dev/test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,130 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# Seeds 
+# 1st section --> A single company (Ekta) to test and develop company-specific
+# functionality -- eg. Team Mgmt, Company Settings
 #
-# Examples:
+# 2nd section --> Another company (Microsoft) to test and develop basic interactions between
+# 2 companies.
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 3rd section --> Multiple companies, to test search, features where we'd like a larger number
+# of companies to utilize
+
+def company_user_association(user, company, role = 5) 
+  RolesUser.create({
+    user_id: user.id,
+    company_id: company.id,
+    role_id: role
+  })
+
+  UsersCompany.create({
+    user_id: user.id,
+    company_id: company.id
+  })
+end
+
+mark = {
+      email: 'mark@ekta.co',
+      full_name: 'Mark Franciose',
+      password: 'markmarkmark'
+}
+
+mark = User.create(mark)
+
+ekta = Company.create({
+  name: 'Ekta',
+  owner_id: mark.id
+})
+
+# mark is owner of Ekta
+company_user_association(mark, ekta, 3)
+
+ekta_users = [
+  {
+    email: 'binbin@ekta.co',
+    full_name: 'BinBin He',
+    password: 'markmarkmark'
+  },
+  {
+    email: 'dominique@ekta.co',
+    full_name: 'Dominique Goncalves',
+    password: 'markmarkmark'
+  },
+  {
+    email: 'mel@ekta.co',
+    full_name: 'Mel',
+    password: 'markmarkmark'
+  },
+  {
+    email: 'alex@ekta.co',
+    full_name: 'Alex Maties',
+    password: 'markmarkmark'
+  },
+  {
+    email: 'kristen@ekta.co',
+    full_name: 'Kristen Madril',
+    password: 'markmarkmark'
+  },
+  {
+    email: 'jim@ekta.co',
+    full_name: 'Jim Barkley',
+    password: 'markmarkmark'
+  }
+]
+
+ekta_users = User.create(ekta_users)
+ekta_users.each do |u|
+  company_user_association(u, ekta, 5)
+end
+
+## Create a second Company to mock Company-to-Company functionality ##
+microsoft = Company.create({
+  name: 'Microsoft'
+})
+
+## Create 10 companies, each with 10 users.
+10.times do
+  owner_user = User.create({
+    full_name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'password1234'
+  })
+
+  company = Company.create({
+    name: Faker::Company.name,
+    owner_id: owner_user.id
+  })
+
+  # setup associations for for owner
+  company_user_association(owner_user, company, 3)
+
+  # create 2 manager users
+  2.times do
+    user = User.create({
+      full_name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: 'password4321'
+    })
+
+    company_user_association(user, company, 4)
+  end
+
+  # create 7 users
+  7.times do
+    user = User.create({
+      full_name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: 'password7890'
+    })
+
+    company_user_association(user, company, 5)
+  end
+
+  # create 4 teams
+  4.times do
+    team = Team.create({
+      name: Faker::Sports::Basketball.team,
+      company_id: company.id
+    })
+  end
+
+end
+
