f80de138b3f2580943f7d0a661f8c334dd22b6fc
Aijaz Khan <aijaz-khan@hotmail.com>
2019-08-30 19:24:31 +0500

    Add more specs for UserType

--- spec/graphql/types/user_type_spec.rb
+++ spec/graphql/types/user_type_spec.rb
@@ -25,9 +25,30 @@ module Types
       expect(subject.fields["displayName"].type.to_type_signature).to eq("String")
       end
 
-    it 'has a :displayName field of String type' do
-      # Ensure the field is of String type
+    it 'has :companies fields of Company type' do
+      # Ensure the field is of Company type
       expect(subject.fields["companies"].type.to_type_signature).to eq("[Company!]")
     end
+
+    it 'has :teams fields of Team type' do
+      # Ensure the field is of Team type
+      expect(subject.fields["teams"].type.to_type_signature).to eq("[Team!]")
+    end
+
+    it 'has :roles fields of RolesUser type' do
+      # Ensure the field is of RolesUser type
+      expect(subject.fields["roles"].type.to_type_signature).to eq("[RolesUser!]")
+    end
+
+    it 'has a :lastViewedCompanyId fields of ID type' do
+      # Ensure the field is of lastViewedCompany type
+      expect(subject.fields["lastViewedCompanyId"].type.to_type_signature).to eq("ID")
+    end
+
+    it 'has a :profile_url fields of String type' do
+      # Ensure the field is of profile_url type
+      expect(subject.fields["profileUrl"].type.to_type_signature).to eq("String")
+    end
+
   end
 end
