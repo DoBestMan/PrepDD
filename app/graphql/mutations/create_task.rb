ec10059ac5529261fb711ce8e84eb49702505e27
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-01 14:43:03 +0500

    Create mutation to add task

---
+++ app/graphql/mutations/create_task.rb
@@ -0,0 +1,35 @@
+class Mutations::CreateTask < GraphQL::Schema::Mutation
+  argument :name, String, required: true
+  argument :description, String, required: false
+  argument :listId, ID, required: true
+  argument :priority, String, required: true
+  argument :status, String, required: true
+
+  field :task, Types::TaskType, null: true
+  field :errors, [Types::FormErrorType], null: false
+  field :success, Boolean, null: false
+
+  def resolve(name: nil, description: nil, list_id: nil, priority: nil, status: nil)
+    response = { errors: [] }
+
+    task = Task.create(name: name, description: description, list_id: list_id,
+                       status: status, priority: priority
+    )
+
+    task.errors.messages.each do |path, messages|
+      messages.each do |message|
+        response[:errors].push(
+          { path: path.to_s.camelcase(:lower), message: message }
+        )
+        response[:success] = false
+      end
+    end
+
+    if list&.persisted?
+      response[:task] = task
+      response[:success] = true
+    end
+
+    response
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < GraphQL::Schema::Object
+    field :createTask, mutation: Mutations::CreateTask
     field :createList, mutation: Mutations::CreateList
     field :createAssociatedCompany, mutation: Mutations::CreateAssociatedCompany
     field :updateCompanySettings, mutation: Mutations::UpdateCompanySettings
