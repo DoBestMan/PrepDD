e3792c00209086dfd4e312598a9edc1b216bff56
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-01 13:32:43 +0500

    Add resolve method to create list

--- app/graphql/mutations/create_list.rb
+++ app/graphql/mutations/create_list.rb
@@ -1,14 +1,38 @@
-module Mutations
-  class CreateList < GraphQL::Schema::RelayClassicMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+class Mutations::CreateList < GraphQL::Schema::Mutation
+  argument :name, String, required: true
+  argument :description, String, required: false
+  argument :requesterId, Id, required: true
+  argument :responderId, Id, required: false
+  argument :isTemplate, Boolean, required: true
+  argument :isPublicTemplate, Boolean, required: true
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+  field :list, Types::ListType, null: true
+  field :errors, [Types::FormErrorType], null: false
+  field :success, Boolean, null: false
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+  def resolve(name: nil, description: nil, requester_id: nil, responder_id: nil, is_template: nil,
+              is_public_template: nil)
+    response = { errors: [] }
+
+    list = List.create(name: name, description: description, requester_id: requester_id,
+                       responder_id: responder_id, is_template: is_template,
+                       is_public_template: is_template
+    )
+
+    list.errors.messages.each do |path, messages|
+      messages.each do |message|
+        response[:errors].push(
+          { path: path.to_s.camelcase(:lower), message: message }
+        )
+        response[:success] = false
+      end
+    end
+
+    if list&.persisted?
+      response[:list] = list
+      response[:success] = true
+    end
+
+    response
   end
 end
