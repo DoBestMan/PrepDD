16e3664fc9b9c61b935f305feafc1b4d067243e5
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-04 00:46:50 +0500

    Make task section association optional

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   enum priority: { low: 0, medium: 1, high: 2 }
   enum status: { backlog: 0, pending: 1, inProgress: 2, completed: 4 }
   belongs_to :list
-  belongs_to :task_section
+  belongs_to :task_section, optional: true
 
   def self.import(file, list_id)
     require 'roo'
