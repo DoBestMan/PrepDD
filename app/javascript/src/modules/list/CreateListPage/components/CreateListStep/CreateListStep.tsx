cd1d9a164e3847801ab95f2108f571a279e0e4ae
doBestMan <Do.best.Man@outlook.com>
2019-09-03 16:44:30 +0200

    add footer for create list step

--- app/javascript/src/modules/list/CreateListPage/components/CreateListStep/CreateListStep.tsx
+++ app/javascript/src/modules/list/CreateListPage/components/CreateListStep/CreateListStep.tsx
@@ -4,11 +4,11 @@ import {Theme, makeStyles, createStyles} from '@material-ui/core/styles';
 import {
   Grid,
   Typography, 
-  FormControl, 
-  FormLabel, 
+  FormControl,
   FormControlLabel,
   RadioGroup, 
   Radio, 
+  Button
 } from '@material-ui/core';
 
 import InternalIcon from '@material-ui/icons/Lock';
@@ -20,13 +20,28 @@ import InputForm from './components/InputForm';
 
 const useStyles = makeStyles((theme: Theme) => 
   createStyles({
-    root: {
-      display: 'block', 
-      margin: '0px calc((100% - 1080px) / 2) 0px calc((100% - 1080px) / 2)', 
+    root: {},
+    body: {
+      height: 'calc(100vh - 168px)',
+      padding: '0px calc((100% - 1080px) / 2) 0px calc((100% - 1080px) / 2)', 
+      borderBottom: '1px solid #D8D8D8',
+      overflow: 'auto', 
     }, 
+    footer: {
+      height: '72px', 
+      padding: '0px calc((100% - 1080px) / 2) 0px calc((100% - 1080px) / 2)', 
+    },
     flex: {
       display: 'flex', 
       alignItems: 'center', 
+    },
+    grow: {
+      flexGrow: 1, 
+    },
+    secondary: {
+      color: '#606060', 
+      marginTop: '48px', 
+      marginBottom: '12px', 
     },
     sharingTitle: {
       color: '#606060', 
@@ -39,6 +54,20 @@ const useStyles = makeStyles((theme: Theme) =>
     explanation: {
       color: '#606060', 
       marginLeft: '32px', 
+    },
+    grayRect: {
+      width: '36px', 
+      height: '36px', 
+      backgroundColor: '#F2F2F2',
+      border: '1px solid #D8D8D8',
+      borderRadius: '3px',
+      marginRight: '12px', 
+    },
+    description: {
+      width: '100%', 
+      height: '180px', 
+      borderRadius: '3px',
+      resize: 'none', 
     }
   })
 );
@@ -72,38 +101,79 @@ export default function CreateListStep(props: CreateListStepProps) {
   )
 
   return stepNumber == currentStep ? (
-    <div className={classes.root}>
-      <Grid container spacing={6}>
-        <Grid item md={6}>
-          <Typography variant="h2">Create List</Typography>
-          <FormControl component="fieldset" style={{marginTop: '48px'}}>
-            <Typography variant="h6" className={classes.sharingTitle}>
-              Sharing
-            </Typography>
-            <RadioGroup
-              aria-label="sharing"
-              name="sharing"
-            >
-              <FormControlLabel value="internal" control={<Radio color="primary" />} label={InternalLabel} />
-              <Typography variant="h6" className={classes.explanation}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    <div>
+      <div className={classes.body}>
+        <Grid container spacing={6}>
+          <Grid item md={6}>
+            <Typography variant="h2">Create List</Typography>
+            <FormControl component="fieldset" style={{marginTop: '48px'}}>
+              <Typography variant="h6" className={classes.sharingTitle}>
+                Sharing
               </Typography>
-              <FormControlLabel value="share" control={<Radio color="primary" />} label={ShareLabel} />
-              <Typography variant="h6" className={classes.explanation}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Typography>
-              <FormControlLabel value="issue" control={<Radio color="primary" />} label={IssueLabel} />
-              <Typography variant="h6" className={classes.explanation}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Typography>
-            </RadioGroup>
-          </FormControl>
+              <RadioGroup
+                aria-label="sharing"
+                name="sharing"
+              >
+                <FormControlLabel value="internal" control={<Radio color="primary" />} label={InternalLabel} />
+                <Typography variant="h6" className={classes.explanation}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Typography>
+                <FormControlLabel value="share" control={<Radio color="primary" />} label={ShareLabel} />
+                <Typography variant="h6" className={classes.explanation}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Typography>
+                <FormControlLabel value="issue" control={<Radio color="primary" />} label={IssueLabel} />
+                <Typography variant="h6" className={classes.explanation}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Typography>
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+          <Grid item md={6}>
+            <Typography variant="h2">List Details</Typography>
+
+            <InputForm label="Title" value="PrepDD Series B" />
+
+            <div>
+              <Typography variant="h6" className={classes.secondary}>Template</Typography>
+              <div className={classes.flex}>
+                <div className={classes.grayRect} />
+                <Typography variant="h6">Series B Diligence</Typography>
+              </div>
+            </div>
+
+            <div>
+              <Typography variant="h6" className={classes.secondary}>List owner(s)</Typography>
+              <div className={classes.flex}>
+                <div className={classes.grayRect} />
+                <Typography variant="h6">Series B Diligence</Typography>
+              </div>
+            </div>
+
+            <div>
+              <Typography variant="h6" className={classes.secondary}>Issue to</Typography>
+              <div className={classes.flex}>
+                <div className={classes.grayRect} />
+                <Typography variant="h6">Series B Diligence</Typography>
+              </div>
+            </div>
+
+            <div>
+              <Typography variant="h6" className={classes.secondary}>Description</Typography>
+              <textarea className={classes.description} />
+            </div>
+          </Grid>
         </Grid>
-        <Grid item md={6}>
-          <Typography variant="h2">List Details</Typography>
-          <InputForm label="Title" value="PrepDD Series B" />
-        </Grid>
-      </Grid>
+      </div>
+      
+      <div className={classes.footer}>
+        <div className={classes.flex} style={{paddingTop: '18px'}}>
+          <div className={classes.grow} />
+          <Button variant="contained">
+            Create List
+          </Button>
+        </div>
+      </div>
     </div>
   ): null;
 };
