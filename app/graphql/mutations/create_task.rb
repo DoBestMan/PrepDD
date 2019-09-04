d9e506033086523f23ceb6d1fca23e64a0f5e3a6
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-05 01:56:17 +0500

    Create task array mutation

--- app/graphql/mutations/create_task.rb
+++ app/graphql/mutations/create_task.rb
@@ -1,36 +1,29 @@
 class Mutations::CreateTask < GraphQL::Schema::Mutation
-  argument :name, String, required: true
-  argument :description, String, required: false
   argument :listId, ID, required: true
-  argument :priority, String, required: true
-  argument :status, String, required: true
-  argument :section, String, required: false
+  argument :tasks, [Types::TaskAttributes ], required: true
 
   field :task, Types::TaskType, null: true
   field :errors, [Types::FormErrorType], null: false
   field :success, Boolean, null: false
 
-  def resolve(name: nil, description: nil, list_id: nil, priority: nil, status: nil, section: nil)
+  def resolve(list_id: nil, tasks: nil)
     response = { errors: [] }
 
-    task = Task.create(name: name, description: description, list_id: list_id,
-                       status: status, priority: priority, section: section
-    )
+    list = List.find(list_id)
 
-    task.errors.messages.each do |path, messages|
-      messages.each do |message|
-        response[:errors].push(
-          { path: path.to_s.camelcase(:lower), message: message }
-        )
-        response[:success] = false
+    tasks.each do |task|
+
+      if task.section.present?
+        task_section = TaskSection.where(name: task.section).first_or_create
+        list.tasks.create(name: task.name, description: task.description, priority: task.priority,
+                          task_section_id: task_section&.id )
+      else
+        list.tasks.create(name: task.name, description: task.description, priority: task.priority)
       end
+
     end
 
-    if list&.persisted?
-      response[:task] = task
-      response[:success] = true
-    end
-
+    response[:success] = true
     response
   end
 end
