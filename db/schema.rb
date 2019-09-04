9fe8ec4ec34c3323e5168a255edf4759eb377a7b
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-05 01:08:17 +0500

    Add team id in list users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_160018) do
+ActiveRecord::Schema.define(version: 2019_09_04_191854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,9 @@ ActiveRecord::Schema.define(version: 2019_09_04_160018) do
     t.bigint "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "team_id"
     t.index ["list_id"], name: "index_lists_users_on_list_id"
+    t.index ["team_id"], name: "index_lists_users_on_team_id"
     t.index ["user_id"], name: "index_lists_users_on_user_id"
   end
 
