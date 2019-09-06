51d85248a22ff5353f148ca374eecdb676d56612
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-06 19:29:56 +0500

    Make list requester optional

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  belongs_to :requester, class_name: 'Company', foreign_key: 'requester_id'
+  belongs_to :requester, class_name: 'Company', foreign_key: 'requester_id', optional: true
   belongs_to :responder, class_name: 'Company', foreign_key: 'responder_id', optional: true
 
   has_many :tasks
