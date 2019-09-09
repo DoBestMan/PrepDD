69d129e624a126c49c765844a630e6dd44f21c2d
doBestMan <Do.best.Man@outlook.com>
2019-09-09 19:48:40 +0200

    redirect user in signin page

--- app/javascript/src/modules/route/auth.tsx
+++ app/javascript/src/modules/route/auth.tsx
@@ -1,7 +1,9 @@
 import LoadingFallback from '../common/LoadingFallback';
 import React, {lazy, Suspense} from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {LinkedInPopUp} from 'react-linkedin-login-oauth2';
+
+import {useGlobalState} from '../../store';
 
 const CreateCompanyPage = lazy(() => import('../auth/CreateCompanyPage'));
 const SignUpPage = lazy(() => import('../auth/SignUpPage'));
@@ -9,12 +11,36 @@ const SignInPage = lazy(() => import('../auth/SignInPage'));
 const ForgotPasswordPage = lazy(() => import('../auth/ForgotPasswordPage'));
 const ResetPasswordPage = lazy(() => import('../auth/ResetPasswordPage'));
 
+const PublicRoute = (props: any) => {
+  const {component: Component, ...rest} = props;
+  const {state} = useGlobalState();
+  const isAuthenticated = state.currentUser.email ? true : false;
+
+  return (
+    <Route
+      {...rest}
+      render={(props: any) =>
+        !isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/app',
+              state: {from: props.location},
+            }}
+          />
+        )
+      }
+    />
+  );
+};  
+
 export default function Router(props: {path: string}) {
   return (
     <Suspense fallback={<LoadingFallback />}>
       <Switch>
         <Route exact path="/signup/:companyName" component={SignUpPage} />
-        <Route exact path="/signin" component={SignInPage} />
+        <PublicRoute exact path="/signin" component={SignInPage} />
         <Route exact path="/forgot" component={ForgotPasswordPage} />
         <Route
           exact
