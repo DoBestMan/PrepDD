8e5a45094714d82e86d212a40a92c05b82b6a30f
doBestMan <Do.best.Man@outlook.com>
2019-09-09 05:04:49 +0200

    make files pane

--- app/javascript/src/modules/task/TaskPage/components/SidePanel/components/FilesPane.tsx
+++ app/javascript/src/modules/task/TaskPage/components/SidePanel/components/FilesPane.tsx
@@ -1,16 +1,82 @@
 import React from 'react';
-import clsx from 'clsx';
-import {Theme, makeStyles, createStyles} from '@material-ui/core/styles';
-import {Paper} from '@material-ui/core';
+import {Theme, createStyles, makeStyles} from '@material-ui/core/styles';
+import {
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableRow, 
+  Paper, 
+  Typography,
+} from '@material-ui/core';
+
+import GetApp from '@material-ui/icons/GetApp';
+
+const Excel = require('images/dummy/logos/excel.svg');
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {},
-    invisible: {
-      display: 'none',
+    root: {
+      padding: '0px 24px 0px 24px',
     },
+    grow: {
+      flexGrow: 1,
+    },
+    flex: {
+      display: 'flex',
+      alignItems: 'center', 
+    },
+    icon: {
+      marginRight: '12px', 
+    },
+    modified: {
+      opacity: 0.5,
+      color: '#606060',
+    },
+    download: {
+      cursor: 'pointer', 
+      marginLeft: '12px', 
+    }
   })
 );
+
+const rows = [
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+  {
+    title: '_title of excel doc_',
+    modified: '12 hours ago',
+  },
+];
 
 interface FilesPaneProps {
   value?: number;
@@ -19,15 +85,39 @@ interface FilesPaneProps {
 
 export default function FilesPane(props: FilesPaneProps) {
   const {value, index} = props;
+
   const classes = useStyles();
 
   return (
     <Paper
-      className={clsx(classes.root, value !== index && classes.invisible)}
-      aria-labelledby="Files"
+      className={classes.root}
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tab-${index}`}
+      aria-labelledby={`tab-${index}`}
       elevation={0}
     >
-      <h1>Files</h1>
+      <Table>
+        <TableBody>
+          {rows.map((row, index) => (
+            <TableRow key={`files-${index}`}>
+              <TableCell align="left">
+                <div className={classes.flex}>
+                  <img className={classes.icon} src={Excel} alt="Excel" /> 
+                  <Typography variant="h6">
+                    {row.title}
+                  </Typography>
+                  <div className={classes.grow} />
+                  <Typography variant="h6" className={classes.modified}>
+                    {row.modified}
+                  </Typography>
+                  <GetApp className={classes.download} />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </Paper>
   );
 }
