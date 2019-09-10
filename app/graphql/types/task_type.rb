241ebb08a4d73047a9709fcc13e5fb8d14ae626a
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-11 03:15:29 +0500

    Add task reviewers in query

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -12,6 +12,7 @@ module Types
     field :isActive, Boolean, null: true
     field :list, ListType, null: true
     field :user_owners, [UserType], null: true
+    field :reviewers, [UserType], null: true
     field :team_owners, [TaskType], null: true
 
     def section
@@ -19,11 +20,15 @@ module Types
     end
 
     def user_owners
-      object.task_owner.where(task_ownerable_type: 'User').map{|task| task.task_ownerable}
+      object.task_owner.where(task_ownerable_type: 'User', owner_type: "Owner").map{|task| task.task_ownerable}
     end
 
     def team_owners
-      object.task_owner.where(task_ownerable_type: 'Team').map{|task| task.task_ownerable}
+      object.task_owner.where(task_ownerable_type: 'Team', owner_type: "Owner").map{|task| task.task_ownerable}
+    end
+
+    def reviewers
+      object.task_owner.where(task_ownerable_type: 'User', owner_type: "Reviewer")
     end
 
   end
