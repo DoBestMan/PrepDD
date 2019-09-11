1675cdc8f14b4a3f1d3922bfb95989a8104922a5
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-11 20:45:18 +0500

    Add task message model

---
+++ app/models/task_message.rb
@@ -0,0 +1,2 @@
+class TaskMessage < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_153424) do
+ActiveRecord::Schema.define(version: 2019_09_11_154027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,16 @@ ActiveRecord::Schema.define(version: 2019_09_11_153424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "task_messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "team_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_task_messages_on_team_id"
+    t.index ["user_id"], name: "index_task_messages_on_user_id"
   end
 
   create_table "task_owners", force: :cascade do |t|
