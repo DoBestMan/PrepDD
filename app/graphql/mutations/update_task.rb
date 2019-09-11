b9c8cdf1ad96b0bd5275b27b24846cd38510aa38
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-11 19:36:36 +0500

    Add more details to task mutation

--- app/graphql/mutations/update_task.rb
+++ app/graphql/mutations/update_task.rb
@@ -3,16 +3,25 @@ class Mutations::UpdateTask < GraphQL::Schema::Mutation
   argument :name, String, required: false
   argument :description, String, required: false
   argument :priority, String, required: false
-
+  argument :status, String, required: false
 
   field :task, Types::TaskType, null: false
   field :errors, [Types::FormErrorType], null: false
   field :success, Boolean, null: false
 
-  def resolve(id: nil, name: nil, description: nil, priority: nil)
+  def resolve(id: nil, name: nil, description: nil, priority: nil, status: nil)
+
     response = { errors: [] }
 
     task = Task.find(id)
+    task.update(
+      {
+        name: name,
+        description: description,
+        priority: priority,
+        status: status
+      }
+    )
     
     if task&.persisted?
       response[:success] = true
