09d80b975cec8f678f140792e3f996fd1d3ed17d
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-13 00:03:29 +0500

    Update remove task owner mutation

--- app/graphql/mutations/remove_task_owners.rb
+++ app/graphql/mutations/remove_task_owners.rb
@@ -1,14 +1,50 @@
-module Mutations
-  class RemoveTaskOwners < GraphQL::Schema::RelayClassicMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+class Mutations::RemoveTaskOwners < GraphQL::Schema::Mutation
+  argument :taskID, ID, required: true
+  argument :userOwners, [String], required: false
+  argument :userReviewers, [String], required: false
+  argument :teamOwners, [ID], required: false
+  argument :teamReviewers, [ID], required: false
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+  field :task, Types::TaskType, null: false
+  field :errors, [Types::FormErrorType], null: false
+  field :success, Boolean, null: false
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+  def resolve(task_id: nil, user_owners: nil, user_reviewers:nil, team_owners: nil, team_reviewers: nil)
+
+    response = { errors: [] }
+
+    task = Task.find(task_id)
+
+    if user_owners.present?
+      user_owners.each do |email|
+        user = User.find_by_email(email)
+        TaskOwner.where(task_id: task_id, task_ownerable_id: user&.id,  task_ownerable_type: "User").first.destroy!
+      end
+    end
+
+    if user_reviewers.present?
+      user_reviewers.each do |email|
+        user = User.find_by_email(email)
+        TaskOwner.where(task_id: task_id, task_ownerable_id: user&.id,  task_ownerable_type: "User").first.destroy!
+      end
+    end
+
+    if team_owners
+      team_owners.each do |id|
+        TaskOwner.where(task_id: task_id, task_ownerable_id: id,  task_ownerable_type: "Team").first.destroy!
+      end
+    end
+
+    if team_reviewers
+      team_owners.each do |id|
+        TaskOwner.where(task_id: task_id, task_ownerable_id: id,  task_ownerable_type: "Team").first.destroy!
+      end
+    end
+
+    response[:success] = true
+    response[:task] = task
+    response
   end
+
 end
+
