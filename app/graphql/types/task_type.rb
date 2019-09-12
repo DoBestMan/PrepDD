37d4159a61ea62064a971c500aea923568130029
Mark Franciose <mark.franciose@gmail.com>
2019-09-11 23:48:57 -0500

    Implement updated_at to return calculated string

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -5,7 +5,7 @@ module Types
     field :id, String, null: false
     field :name, String, null: true
     field :description, String, null: true
-    field :updated_at, GraphQL::Types::BigInt, null: true, method: :adjust_epoch
+    field :updated_at, String, null: true, method: :last_updated_at
     field :priority, String, null: true
     field :status, String, null: true
     field :due_date, String, null: true
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,22 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def last_updated_at
+    diff = Time.now - self.updated_at
+    case diff
+    when 0..60
+      return "just now"
+    when 61..3600
+      diff = (diff / 60).to_i
+      unit = 'minute'.pluralize diff
+    when 3601..(3600 * 24)
+      diff = (diff / 3600).to_i
+      unit = 'hour'.pluralize diff
+    else 
+      diff = (diff / 3600 * 24).to_i
+      unit = 'day'.pluralize diff
+    end
+    "#{diff} #{unit} ago"
+  end
+
 end
