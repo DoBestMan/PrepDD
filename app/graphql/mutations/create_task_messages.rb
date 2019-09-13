deaf45ac12b4bd448407add6dc30726b2f10c622
Aijaz Khan <aijaz-khan@hotmail.com>
2019-09-13 14:28:54 +0500

    Create task message

--- app/graphql/mutations/create_task_messages.rb
+++ app/graphql/mutations/create_task_messages.rb
@@ -1,14 +1,29 @@
-module Mutations
-  class CreateTaskMessages < GraphQL::Schema::RelayClassicMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+class Mutations::CreateTaskMessages < GraphQL::Schema::Mutation
+  argument :taskId, ID, required: true
+  argument :userId, ID, required: true
+  argument :message, String, required: true
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+  field :message, Types::TaskMessageType, null: true
+  field :errors, [Types::FormErrorType], null: false
+  field :success, Boolean, null: false
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+  def resolve(task_id: nil, user_id: nil, message: nil)
+    response = { errors: [] }
+
+    task_message = TaskMessage.create(task_id: task_id, user_id: user_id, message: message)
+
+    task_message.errors.messages.each do |path, messages|
+      messages.each do |message|
+        response[:errors].push(
+          { path: path.to_s.camelcase(:lower), message: message }
+        )
+        response[:success] = false
+      end
+    end
+
+    if task_message&.persisted?
+      response[:success] = true
+    end
+    response
   end
 end
